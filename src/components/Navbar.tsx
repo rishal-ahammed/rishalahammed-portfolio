f4b074ae5ae1229d3ce5f88ad7f9ce7255393bba
Rishal <rishalahmd6@gmail.com>
2025-05-24 16:51:59 +0530

    Navbar name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,7 +57,7 @@ const Navbar: React.FC = () => {
                 href="#home"
                 className="text-2xl font-bold font-display text-primary-700 dark:text-primary-300"
               >
-                Portfolio
+                RISHAL.
               </a>
             </motion.div>
 
